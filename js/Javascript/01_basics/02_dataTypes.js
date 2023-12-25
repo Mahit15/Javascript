e1e93ca2eeadceb5e6777e0b90f2c5cb925f8f77
Mahit15 <115122762+Mahit15@users.noreply.github.com>
2023-12-25 16:56:56 +0000

    02 datatypes

---
+++ js/Javascript/01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // can only use this in browser not nodejs
+
+let name = "Mahit" //string
+let age = 18 //number
+let isLoggedIn = false //boolean
+
+// null -> standalone value 
+// undefined
+// symbol -> uniqueness
+// object
+
+
+// typeof keyword used to find type of variable
+// null is object type
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
