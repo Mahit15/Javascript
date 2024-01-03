a008b9c773b42975c26b8db3aeb699cb1bf2acec
Mahit15 <115122762+Mahit15@users.noreply.github.com>
2024-01-03 01:32:41 +0000

    functions 2

--- js/Javascript/03_basics/01_functions.js
+++ js/Javascript/03_basics/01_functions.js
@@ -30,4 +30,33 @@ function loginUserMessage(username = "sam"){ //sam becomes default value if noth
     return `${username} just logged in`;
 }
 //console.log(loginUserMessage("Mahit"));
-console.log(loginUserMessage());
+//console.log(loginUserMessage());
+
+
+function calculateCartPrice(val1 ,val2, ...num1){ // ... is rest operator, function can return multiple arguments as array
+    return num1;
+}
+// console.log(calculateCartPrice(200, 400, 500, 600, 700));
+
+const user = {
+    username: "Mahit",
+    price: 99
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+// handleObject(user);
+
+handleObject({ //directly defining the object in function call 
+    username: "sam",
+    price: 199
+})
+
+const myNewArray = [200, 400, 500, 100, 200]
+
+function returnSecondValue(arr){
+    return arr[1];
+}
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 100, 200]));
