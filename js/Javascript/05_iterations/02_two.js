85d6b5349a093f5a7ab9d18b92213605f43b34cd
Mahit15 <115122762+Mahit15@users.noreply.github.com>
2024-01-07 22:25:57 +0000

    while, do while

---
+++ js/Javascript/05_iterations/02_two.js
@@ -0,0 +1,21 @@
+//while 
+
+let index = 0
+while(index <= 10){
+    console.log(`Value of index is ${index}`);
+    index += 2
+}
+
+let myArr = ["fives", "rex", "cody"]
+
+let arr = 0
+while(arr < myArr.length){
+    console.log(`Value is ${myArr[arr]}`);
+    arr++;
+}
+
+let score = 1
+do{
+    console.log(`Score is ${score}`);
+    score++
+} while(score < 10);
