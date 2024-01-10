16ccef59b3872661b53020f185d700242f4e4ea1
Mahit15 <115122762+Mahit15@users.noreply.github.com>
2024-01-10 14:05:01 +0000

    reduce function

--- js/Javascript/05_iterations/08_eight.js
+++ js/Javascript/05_iterations/08_eight.js
@@ -1,8 +1,33 @@
 const myNums = [1, 2, 3]
 
-const myTotal = myNums.reduce(function (acc, currval){ //accumulator and current value
-  console.log(`Accumulator: ${acc}, Current value: ${currval}`);
-  return acc + currval;
-}, 0) //value after the comma will be initial value of accumulator
+// const myTotal = myNums.reduce(function (acc, currval){ //accumulator and current value
+//   console.log(`Accumulator: ${acc}, Current value: ${currval}`);
+//   return acc + currval; //returned value becomes next value of accumulator
+// }, 0) //value after the comma will be initial value of accumulator
 
+
+
+const myTotal = myNums.reduce( (acc, curr) => acc + curr, 0)
 console.log(myTotal);
+
+const shoppingCart = [
+  {
+    itemName: "js course",
+    price: 2999,
+  },
+  {
+    itemName: "python",
+    price: 999,
+  },
+  {
+    itemName: "mobile app dev",
+    price: 4999,
+  },
+  {
+    itemName: "data science course",
+    price: 9999,
+  }
+]
+
+const price = shoppingCart.reduce( (acc, item) => (acc + item.price), 0);
+console.log(price);
