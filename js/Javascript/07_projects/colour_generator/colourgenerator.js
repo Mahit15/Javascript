b99260669d649b639764ecca8bc6490d383be702
Mahit15 <115122762+Mahit15@users.noreply.github.com>
2024-01-23 14:22:33 +0530

    Create colourgenerator.js

---
+++ js/Javascript/07_projects/colour_generator/colourgenerator.js
@@ -0,0 +1,27 @@
+//generating random colour
+
+const randomColour = function(){
+  const hex = "0123456789ABCDEF"
+  let colour = '#'
+  for(let i = 0; i < 6; i++){
+      colour += hex[Math.floor(Math.random() * 16)]
+      
+  }
+  return colour;
+};
+
+let intervalId;
+const startColourChange = function(){
+  
+  function changeBgcolour(){
+    document.body.style.backgroundColor = randomColour()
+  }
+  intervalId = setInterval(changeBgcolour, 1000)
+}
+const stopColourChange = function(){
+  clearInterval(intervalId);
+  intervalId = null
+}
+document.getElementById("start").addEventListener('click', startColourChange);
+
+document.getElementById("stop").addEventListener('click', stopColourChange);
